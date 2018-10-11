49423cd3dc9842df580dfde96f316e50c6654f74
moruo44 <734733711@qq.com>
2018-10-11 14:52:46 +0800

    feat: 完成功能

---
+++ content_script/highlighter.js
@@ -0,0 +1,110 @@
+(function() {
+    let keywordStr = window.localStorage.getItem('highlight__mor__keywords')
+    let keywords = keywordStr ? JSON.parse(keywordStr) : []
+    let reg = new RegExp(keywords.join('|'), 'g')
+
+    let _switch = window.localStorage.getItem('highlight__mor__switch') || 'on'
+
+    const highLightTagHead = '<span\
+        style="background: rgb(255, 198, 0);\
+            border-radius: 3px;\
+            box-shadow: rgba(0, 0, 0, 0.3) 1px 1px 3px;\
+            display:inline;\
+            color: black;\
+            padding: 0 2px; "\
+        data-highlighted="1">'
+    const highLightTagTail = '</span>'
+    let hightedElements = []
+    let mutationByCancel = false
+
+    function highLight(content) {
+        if (mutationByCancel) {
+            mutationByCancel = false
+            return // 取消高亮操作导致的Mutation，直接返回
+        }
+        if (_switch === 'off') return
+        if (keywords.length === 0) return
+
+        const all = document.all
+        const len = all.length
+
+        const excludeTagName = ['STYLE', 'LINK', 'SCRIPT', 'META', 'TITLE']
+        for (let i = 0; i < len; i++) {
+            const element = all[i]
+
+            ;[...element.childNodes]
+                .filter(node => node.nodeType === 3 
+                            && !node.parentNode.dataset.highlighted // 被高亮之后需要标记，否则会无限循环
+                            && !excludeTagName.includes(node.tagName)
+                            && document.body.contains(node))
+                .forEach(textNode => {
+                    const text = textNode.data
+                    if (text.search(reg) === -1) return
+
+                    const newElement = document.createElement('morun')
+                    newElement.innerHTML = text.replace(reg, match => `${highLightTagHead}${match}${highLightTagTail}`)
+                    textNode.replaceWith(newElement)
+                    hightedElements.push(newElement)
+                })
+        }
+    }
+
+    function cancelHight() {
+        if (hightedElements.length === 0) return
+        hightedElements.forEach(element => {
+            try {
+                element.outerHTML = element.innerText
+            } catch (e) {
+                console.log(e, element)
+            }
+        })
+        hightedElements = []
+        mutationByCancel = true
+    }
+
+    const mo = new MutationObserver(highLight)
+    const options = {
+        'childList': true,
+        'characterData': true,
+        'subtree': true,
+    }
+    mo.observe(document.body, options)
+
+    // 和popup通信
+    chrome.runtime.onMessage.addListener(function({cmd, keywordList, regStr}, sender, sendResponse){
+        if(cmd === 'highlight__mor__updatekeyword') {
+            keywords = keywordList
+            window.localStorage.setItem('highlight__mor__keywords', JSON.stringify(keywords))
+
+            reg = new RegExp(regStr, 'g')
+            cancelHight()
+            highLight()
+        }
+
+        if (cmd === 'highlight__mor__getsetting') {
+            sendResponse({
+                cmd: 'highlight__mor__getsetting',
+                keywords,
+                _switch,
+            })
+        }
+
+        if (cmd === 'highlight__mor__offhight') {
+            cancelHight()
+            _switch = 'off'
+            window.localStorage.setItem('highlight__mor__switch', 'off')
+        }
+
+        if (cmd === 'highlight__mor__onhight') {
+            _switch = 'on'
+            window.localStorage.setItem('highlight__mor__switch', 'on')
+            highLight()
+        }
+
+        if (cmd === 'highlight__mor__clearkeyword') {
+            keywords = []
+            window.localStorage.removeItem('highlight__mor__keywords')
+            cancelHight()
+        }
+    })
+})()
---
+++ popup/popup.js
@@ -0,0 +1,65 @@
+function sendMessageToContentScript(message, callback) {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+            if (callback) callback(response);
+        })
+    })
+}
+
+let highlightSwitch = 'on'
+const switchBar = document.querySelector('.bar')
+document.querySelector('.switch').addEventListener('click', function (e) {
+    if (highlightSwitch === 'on') {
+        highlightSwitch = 'off'
+        switchBar.classList.remove('on')
+        sendMessageToContentScript({
+            cmd: 'highlight__mor__offhight',
+        })
+    } else {
+        highlightSwitch = 'on'
+        switchBar.classList.add('on')
+        sendMessageToContentScript({
+            cmd: 'highlight__mor__onhight',
+        })
+    }
+})
+
+document.getElementById('reset').addEventListener('click', function (e) {
+    document.querySelector('[name=keywords').value = ''
+    sendMessageToContentScript({
+        cmd: 'highlight__mor__clearkeyword',
+    })
+})
+
+document.getElementById('confirm').addEventListener('click', function (e) {
+    const value = document.querySelector('[name=keywords').value.trim()
+    if (!value) return
+
+    const keywordList = value.split(/\s/)
+    const regStr = keywordList.join('|')
+
+    sendMessageToContentScript({
+        cmd: 'highlight__mor__updatekeyword',
+        regStr,
+        keywordList,
+    })
+})
+
+sendMessageToContentScript({
+    cmd: 'highlight__mor__getsetting',
+}, function ({cmd, keywords, _switch}) {
+    if (cmd === 'highlight__mor__getsetting') {
+        document.querySelector('[name=keywords]').value = keywords.join('\n')
+        highlightSwitch = _switch
+        if (highlightSwitch === 'off') {
+            switchBar.classList.remove('transition')
+            switchBar.classList.remove('on')
+            setTimeout(() => {
+                switchBar.classList.add('transition')
+            }, 100)
+        }
+    }
+})
